99f2074ff453a03c9392337a29337f050e76d220
Christian Bautista <christianbautista_2226250002@mhs.mdp.ac.id>
2025-09-26 20:40:08 +0700

    BasicKotilin

--- src/LatihanFunction.kt
+++ src/LatihanFunction.kt
@@ -1,9 +1,5 @@
 fun hitungPanjangNama (name : String?) : Int {
-    return if(name != null) {
-        name.length
-    } else {
-        0
-    }
+    return name?.length ?: 0
 }
 fun main() {
     println(hitungPanjangNama("christian"))
